9ad0985b8f789ce148a6a44e6c57f7d042599b2b
soriiieee <sorimachi.yuichi@jwa.or.jp>
2020-07-15 14:57:34 +0900

    second

--- src/0709_getStockByPandas.py
+++ src/0709_getStockByPandas.py
@@ -28,32 +28,39 @@ from tqdm import tqdm
 # data = quandl.get('WIKI/PRICES',ticker = ['AAPL', 'MSFT', 'WMT'],
 #   qopts = { 'columns': ['ticker', 'date', 'adj_close'] },
 #   date = { 'gte': '2015-12-31', 'lte': '2016-12-31' },
-#   paginate=True)
+#   paginate=True
+def getStockData(ticker, media="yahoo", start_date="201701010000"):
+  yy=start_date[0:4]
+  mm=start_date[4:6]
+  dd=start_date[6:8]
 
-# print(datetime.now().strftime("%Y-%m-%d"))
-# sys.exit()
-symbols = get_nasdaq_symbols()
-datum = web.DataReader("GOOGL","yahoo","2019/11/1")
-print(datum.tail())
-sys.exit()
-def getStockData(sub, ticker):
-  start_date = "2010-01-01"
-  end_date = datetime.now().strftime("%Y-%m-%d")
-  df = quandl.get(f"{sub}/{ticker}",start_date=start_date,end_date=end_date)
-  return df
+  start_fmt=f"{yy}/{mm}/{dd}"
+  
+  data = web.DataReader(ticker,media,start_fmt)
+  print(ticker,data.shape)
+  print(data.head(2))
+  print(data.tail(2))
+  return data
+  # sys.exit()
 
 if __name__ == "__main__":
-  SUB="WIKI"
-  TICKER="AAPL.4"
-  DAT="/home/griduser/work/sori-py2/stock/dat"
+  #
+      # symbols = get_nasdaq_symbols()
+      # use_cols = ['Nasdaq Traded', 'Security Name', 'Listing Exchange', 'Market Category','ETF', 'Round Lot Size', 'Test Issue', 'Financial Status', 'CQS Symbol','NASDAQ Symbol', 'NextShares']
+      # symbols = symbols.reset_index()
+      # symbols.to_csv("../dat/tickers.csv")
+      # # print(datetime.now().strftime("%Y-%m-%d"))
+      # sys.exit()
 
-  DAT_DIR=f"{DAT}/{SUB}"
-  os.makedirs(DAT_DIR, exist_ok=True)
-
-  df = getStockData(SUB, TICKER)
-
-  df.to_csv(f"{DAT_DIR}/{TICKER}.csv")
-  print(df.shape)
-  print(df.tail())
-  sys.exit()
-
+  os.makedirs("../dat/0715" , exist_ok=True)
+  _ticker=["AAPL","MSFT","NVDA","INTC","TSM","SAP","QCOM","DCM","CTSH","EMC"]
+  media="yahoo"
+  start_date="201701010000"
+  for ticker in _ticker:
+    df = getStockData(ticker, media=media, start_date=start_date)
+    df = df.reset_index()
+    df.to_csv(f"../dat/0715/{ticker}.csv", index=False)
+    # print()
+    # print(df.shape)
+    # sys.exit()
+    
